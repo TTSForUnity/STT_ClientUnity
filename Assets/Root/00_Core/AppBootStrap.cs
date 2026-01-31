869c3cd636393c244ad8f6f17f1e39d5878277fd
mundi207 <alsrb6008@gmail.com>
2026-01-31 10:32:45 +0900

    🚀Feat : 싱글톤 부모객체 및 매니저 관리 모듈 추가

---
+++ Assets/Root/00_Core/AppBootStrap.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class AppBootStrap : Singleton<AppBootStrap>
+{
+    
+}
+    
---
+++ Assets/Root/02_Managers/Singleton.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static readonly object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock) // 메인 쓰레드에서만 객체 생성, 참고 타입만 열쇠로 사용 (예 : Object)
+            {
+                if(_instance == null)
+                {
+                    _instance = (T)FindFirstObjectByType(typeof(T));
+
+                    if(_instance == null)
+                    {
+                        GameObject singleton = new GameObject();
+                        _instance = singleton.AddComponent<T>();
+                        singleton.name = typeof(T).ToString() + " (Singleton)";
+
+                        DontDestroyOnLoad(singleton);
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
